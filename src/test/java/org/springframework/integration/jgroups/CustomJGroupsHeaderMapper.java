c581abb4f7d53716ea6fb8436fac795e70e21a90
bharat3108 <bharat3108@gmail.com>
2016-06-01 17:44:09 +0530

    Migrating to version 4.x of SI

--- src/test/java/org/springframework/integration/jgroups/CustomJGroupsHeaderMapper.java
+++ src/test/java/org/springframework/integration/jgroups/CustomJGroupsHeaderMapper.java
@@ -18,7 +18,7 @@ package org.springframework.integration.jgroups;
 import java.util.Map;
 
 import org.jgroups.Message;
-import org.springframework.integration.MessageHeaders;
+import org.springframework.messaging.MessageHeaders;
 
 public class CustomJGroupsHeaderMapper implements JGroupsHeaderMapper {
 
